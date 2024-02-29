de1c73a31afce45955de601ca8d904f5eecde814
hamada <hamadahandoush9991@gmail.com>
2024-02-29 22:18:17 +0200

    version one

---
+++ script/login.js
@@ -0,0 +1,16 @@
+let form = document.getElementById("loginForm");
+
+form.addEventListener("submit", function (e) {
+  let username = document.getElementById("username").value;
+  let password = document.getElementById("password").value;
+  let error = document.getElementById("error");
+  let msg;
+  e.preventDefault();
+  console.log(username);
+  if (username === "AdminSEF123" && password === "SeF@ctORy$$456") {
+    window.location.replace("../pages/main.html");
+  } else {
+    msg = "wrong pass or email please try again!";
+    error.innerText = msg;
+  }
+});
---
+++ script/main.js
@@ -0,0 +1,107 @@
+let input = document.querySelector(".input");
+let submit = document.querySelector(".add");
+let tasksDiv = document.querySelector(".tasks");
+
+// Empty Array To Store The Tasks
+let arrayOfTasks = [];
+
+// Check if Theres Tasks In Local Storage
+if (localStorage.getItem("tasks")) {
+  arrayOfTasks = JSON.parse(localStorage.getItem("tasks"));
+}
+
+// Trigger Get Data From Local Storage Function
+getDataFromLocalStorage();
+
+// Add Task
+submit.onclick = function () {
+  if (input.value !== "") {
+    addTaskToArray(input.value); // Add Task To Array Of Tasks
+    input.value = ""; // Empty Input Field
+  }
+};
+
+tasksDiv.addEventListener("click", (e) => {
+  if (e.target.classList.contains("del")) {
+    // Remove Task From Local Storage
+    deleteTaskWith(e.target.parentElement.getAttribute("data-id"));
+    // Remove Element From Page
+    e.target.parentElement.remove();
+  }
+  // Task Element
+  if (e.target.classList.contains("task")) {
+    // Toggle Completed For The Task
+    toggleStatusTaskWith(e.target.getAttribute("data-id"));
+    // Toggle Done Class
+    e.target.classList.toggle("done");
+  }
+});
+
+function addTaskToArray(taskText) {
+  // Task Data
+  const task = {
+    id: Date.now(),
+    title: taskText,
+    completed: false,
+  };
+
+  arrayOfTasks.push(task);
+
+  addElementsToPageFrom(arrayOfTasks);
+  // Add Tasks To Local Storage
+  addDataToLocalStorageFrom(arrayOfTasks);
+}
+
+function addElementsToPageFrom(arrayOfTasks) {
+  tasksDiv.innerHTML = "";
+
+  arrayOfTasks.forEach((task) => {
+    // Create Main Div
+    let div = document.createElement("div");
+    div.className = "task";
+
+    // Check If Task is Done
+    if (task.completed) {
+      div.className = "task done";
+    }
+    div.setAttribute("data-id", task.id);
+    div.appendChild(document.createTextNode(task.title));
+    // Create Delete Button
+    let span = document.createElement("span");
+    span.className = "del";
+    span.appendChild(document.createTextNode("Delete"));
+    div.appendChild(span);
+
+    tasksDiv.appendChild(div);
+  });
+}
+
+function addDataToLocalStorageFrom(arrayOfTasks) {
+  window.localStorage.setItem("tasks", JSON.stringify(arrayOfTasks));
+}
+
+function getDataFromLocalStorage() {
+  let data = window.localStorage.getItem("tasks");
+  if (data) {
+    let tasks = JSON.parse(data);
+    addElementsToPageFrom(tasks);
+  }
+}
+
+function deleteTaskWith(taskId) {
+  arrayOfTasks = arrayOfTasks.filter((task) => task.id != taskId);
+  addDataToLocalStorageFrom(arrayOfTasks);
+}
+
+function toggleStatusTaskWith(taskId) {
+  for (let i = 0; i < arrayOfTasks.length; i++) {
+    if (arrayOfTasks[i].id == taskId) {
+      if (arrayOfTasks[i].completed == false) {
+        arrayOfTasks[i].completed = true;
+      } else {
+        arrayOfTasks[i].completed = false;
+      }
+    }
+  }
+  addDataToLocalStorageFrom(arrayOfTasks);
+}
